78e9ad13dcf7ad5b8f23f21a79098a834432d817
Allison D <aadwyre@gmail.com>
2023-04-23 20:04:29 -0500

    create more tests

--- test/Rent.js
+++ test/Rent.js
@@ -38,22 +38,44 @@ describe('Rent', () => {
             expect(result).to.be.equal(book.address)
         })
         it('Returns renter', async () => {
-       
+            const result = await rent.renter()
+            expect(result).to.be.equal(renter.address)
         })
         it('Returns bookstore', async () => {
-            
+            const result = await rent.bookstore()
+            expect(result).to.be.equal(bookstore.address)
         })
     })
 
     // Check-out book
+    describe('Check-out', () => {
+        it('Updates checkout status to true', async () => {
+            const transaction = await rent.connect(renter).checkOut(1)
+            await transaction.wait()
+            const result = await rent.isCheckedOut(1)
+            expect(result).to.be.equal(true)
+        })
+    })
 
     // Check-in book
+    describe('Check-in', () => {
+        beforeEach(async () => {
+            const transaction = await rent.connect(renter).checkIn(1)
+            await transaction.wait()
+        })
+        it('Updates checkout status to false', async () => {
+            const result = await rent.isCheckedOut(1)
+            expect(result).to.be.equal(false)
+        })
+        it('Sets duration of rental', async () => {
+            const result = await rent.duration(1)
+            expect(result).to.be.greaterThan(0)
+        })
+        it('Sets amount due', async () => {
+            const result = await rent.amountDue(1)
+            expect(result).to.be.greaterThan(0)
+        })
+    })
 
-    // Get total duration of book use
-
-    // Get Contract Balance
-
-    // Get Renter's balance
-
-    // Set Due amount
+    
 })
