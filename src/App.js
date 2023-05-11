5de359639c39c48eb50408c50f9b0173ba1d5258
Allison D <aadwyre@gmail.com>
2023-05-11 16:30:23 -0500

    update car data

--- src/App.js
+++ src/App.js
@@ -1,13 +1,49 @@
 import './index.css';
+import { useEffect, useState } from 'react';
+import { ethers } from 'ethers';
+import carMetadata from '../../metadata/carMetadata';
 
 // Components
 import Navbar from './components/Navbar';
 import Card from './components/Card';
 
+// ABIs
+import Rent from './abis/Rent.json'
+
 // Config
 import config from './config.json';
 
+
 function App() {
+  const [rent, setRent] = useState(null);
+  const [account, setAccount] = useState(null)
+  const [cars, setCars] = useState([])
+
+
+  const loadBlockchainData = async () => {
+
+    setCars(carMetadata)
+    
+    const provider = new ethers.providers.Web3Provider(window.ethereum)
+    setProvider(provider)
+    const network = await provider.getNetwork()
+
+    const rent = new ethers.Contract(config[network.chainId].rent.address, Rent, provider)
+    setRent(rent)
+
+    
+
+
+    window.ethereum.on('accountsChanged', async () => {
+      const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+      const account = ethers.utils.getAddress(accounts[0])
+      setAccount(account);
+    })
+  }
+
+  useEffect(() => {
+    loadBlockchainData()
+  }, [])
 
   return (
     <div className="App">
