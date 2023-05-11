a942a828db54b48fde742a35c9dcebaf2eddb05a
Allison D <aadwyre@gmail.com>
2023-05-11 11:51:34 -0500

    remove unnecessary arguments

--- contracts/Rent.sol
+++ contracts/Rent.sol
@@ -2,20 +2,19 @@
 
 pragma solidity ^0.8.0;
 
-interface IERC721 {
-    function transferFrom(
-        address _from,
-        address _to,
-        uint256 _id
-    ) external;
-}
+// interface IERC721 {
+//     function transferFrom(
+//         address _from,
+//         address _to,
+//         uint256 _id
+//     ) external;
+// }
 
 contract Rent {
 
   address public owner;
 
   struct Renter {
-    address payable walletAddress;
     string firstName;
     string lastName;
     bool canRent;
@@ -25,26 +24,31 @@ contract Rent {
     uint256 endTime;
   }
 
-  struct Rental {
-    bool isCheckedOut;
-    uint256 price;
-    Renter renter;
-  }
+  // struct Rental {
+  //   bool isCheckedOut;
+  //   uint256 price;
+  //   Renter renter;
+  // }
 
   // renters mapping with renter wallet address as key
   mapping (address => Renter) public renters;
 
   // rentals mapping with book nftId as key
-  mapping (uint256 => Rental) public rentals;
+  //mapping (uint256 => Rental) public rentals;
 
   constructor() {
     owner = msg.sender;
 
   }
 
+  // Able to rent
+  modifier canRentBook(){
+    require (renters[msg.sender].canRent == true);
+    _;
+  }
+
   // Add renter
   function addRenter(
-    address payable _walletAddress,
     string memory _firstName,
     string memory _lastName,
     bool _canRent,
@@ -52,29 +56,27 @@ contract Rent {
     uint256 _amountDue,
     uint256 _startTime,
     uint256 _endTime) public {
-      renters[_walletAddress] = Renter(_walletAddress, _firstName, _lastName, _canRent, _active, _amountDue, _startTime, _endTime);
+      renters[msg.sender] = Renter(_firstName, _lastName, _canRent, _active, _amountDue, _startTime, _endTime);
     }
   // Check-out book
-  function checkOut(address _walletAddress) public {
+  function checkOut() public {
     // set renter to active, add start time, and make unable to rent anything else
-    renters[_walletAddress].active = true;
-    renters[_walletAddress].startTime = block.timestamp;
-    renters[_walletAddress].canRent = false;
+    renters[msg.sender].active = true;
+    renters[msg.sender].startTime = block.timestamp;
+    renters[msg.sender].canRent = false;
     //rentals[_nftAddress] = Rental(true, 1, _walletAddress);
   }
   // Check-in book
-  function checkIn(
-      address _walletAddress
-    ) public {
+  function checkIn() public {
       // set renter to active, add start time, and make unable to rent anything else
-      renters[_walletAddress].active = false;
-      renters[_walletAddress].endTime = block.timestamp;
+      renters[msg.sender].active = false;
+      renters[msg.sender].endTime = block.timestamp;
       // set amount due
-      setDue(_walletAddress);
+      setDue();
     }
   // Get total duration of book use
-  function getTotalTime(address _walletAddress) public view returns(uint){
-    uint total = renters[_walletAddress].endTime - renters[_walletAddress].startTime;
+  function getTotalTime() public view returns(uint){
+    uint total = renters[msg.sender].endTime - renters[msg.sender].startTime;
     uint totalHours = total/60/60;
     return  totalHours;
   }
@@ -84,12 +86,9 @@ contract Rent {
     return address(this).balance;
   }
   // Set Due amount
-  function setDue(address _walletAddress) internal {
-    uint totalHours = getTotalTime(_walletAddress);
-    renters[_walletAddress].amountDue = totalHours * 500000000000000;
+  function setDue() internal {
+    uint totalHours = getTotalTime();
+    renters[msg.sender].amountDue = totalHours * 500000000000000;
   }
-  // Check if able to rent
-  function canRentBook(address _walletAddress) public view returns(bool){
-    return renters[_walletAddress].canRent;
-  }
+
 }
